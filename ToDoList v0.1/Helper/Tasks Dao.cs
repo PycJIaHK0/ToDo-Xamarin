c54537ad315477c0eef1b0b6d3541a9d57b85d35
SergiyPZAS_31 <sergiubudenchuk@gmail.com>
2017-06-06 11:53:44 +0300

    Create v1

---
+++ ToDoList v0.1/Helper/Tasks Dao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Database.Sqlite;
+using Android.Database;
+
+namespace ToDoList_v0._1.Helper
+{
+    class DbHelper : SQLiteOpenHelper
+    {
+       
+
+
+    }
+}
---
+++ ToDoList v0.1/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Content;
+using System;
+using ToDoList_v0._1.Helper;
+using System.Collections.Generic;
+
+namespace ToDoList_v0._1
+{
+    [Activity(Label = "ToDoList_v0._1", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/ThemeOverlay.AppCompat.Light")]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText taskEditText;
+        DbHelper dbHelper;
+        CustomAdapter adapter;
+        ListView lstTask;
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_item, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+      
+        }
+    }
+
+
